34b9dd52498be334d644ac638e45eeeaee63fdd3
lray <lraypublic@163.com>
2014-12-25 22:12:03 +0800

    init project

---
+++ LeetCode/src/leetcode/LeetCode.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014 lray.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    lray - initial API and implementation and/or initial documentation
+ */
+
+package leetcode;
+
+/**
+ * @author  lray
+ * @date    2014-12-25
+ * @blog    http://lray.pw
+ * @mail    lraypublic@163.com
+ * @info    
+ */
+public class LeetCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
